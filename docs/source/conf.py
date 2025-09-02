84f665ac25efd675cc7115f62262514061531549
Mikael2983 <mikael.picolo@laposte.net>
2025-09-02 13:36:35 +0200

    changement de theme readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,5 +26,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'python_docs_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
